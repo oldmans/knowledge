e081fb343cbb6086289b34db3096d23f0ce1ae99
liuyanjie <lyj8888888888@gmail.com>
2018-07-30 16:23:34 +0800

    update python

---
+++ langs/python/demo.py
@@ -0,0 +1,68 @@
+
+class Property(object):
+    "Emulate PyProperty_Type() in Objects/descrobject.c"
+
+    def __init__(self, fget=None, fset=None, fdel=None, doc=None):
+        self.fget = fget
+        self.fset = fset
+        self.fdel = fdel
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+        self.__doc__ = doc
+
+    def __get__(self, obj, objtype=None):
+        if obj is None:
+            return self
+        if self.fget is None:
+            raise AttributeError("unreadable attribute")
+        return self.fget(obj)
+
+    def __set__(self, obj, value):
+        if self.fset is None:
+            raise AttributeError("can't set attribute")
+        self.fset(obj, value)
+
+    def __delete__(self, obj):
+        if self.fdel is None:
+            raise AttributeError("can't delete attribute")
+        self.fdel(obj)
+
+    def getter(self, fget):
+        return type(self)(fget, self.fset, self.fdel, self.__doc__)
+
+    def setter(self, fset):
+        return type(self)(self.fget, fset, self.fdel, self.__doc__)
+
+    def deleter(self, fdel):
+        return type(self)(self.fget, self.fset, fdel, self.__doc__)
+
+class RevealAccess(object):
+    """
+    A data descriptor that sets and returns values
+    normally and prints a message logging their access.
+    """
+
+    def __init__(self, init=None, name='var'):
+        self.name = name
+        self.val = init
+
+    def __get__(self, obj, type=None):
+        print('Retrieving', self.name)
+        return self.val
+
+    def __set__(self, obj, val):
+        print('Updating', self.name)
+        self.val = val
+
+x = RevealAccess(10, 'var "x"')
+
+class MyClass(object):
+    x1 = x
+    x2 = x
+    y = 5
+
+m = MyClass()
+
+m.x1 = 123
+print(m.x1)
+print(m.x2)
---
+++ langs/python/descriptor.md
@@ -0,0 +1,5 @@
+# Descriptor
+
+* [HowTo/descriptor](https://docs.python.org/3/howto/descriptor.html)
+* [Understanding-python-metaclasses](https://blog.ionelmc.ro/2015/02/09/understanding-python-metaclasses)
+* [DataModel](https://docs.python.org/3/reference/datamodel.html)
